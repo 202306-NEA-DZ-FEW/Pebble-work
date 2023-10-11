c66f010a565bb95db1bd2db9aa4b98aef3ee1c2f
Polichinell <tsuqinoa@gmail.com>
2023-10-12 01:10:01 +0200

    feat(event detail page): add the basic structure of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ['var(--font-rubik)'],
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")],
 }
