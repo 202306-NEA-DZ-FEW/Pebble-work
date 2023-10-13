b9fb8c3973cae1c7f6dcd5c67bc21c6fddc54025
takidilmi <angeldilmi@gmail.com>
2023-10-13 04:41:48 +0200

    feat(layout): create footer and Navbar along with the test

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer
+            data-testid='footer'
+            className='bottom-0 md:flex md:items-center md:justify-center flex items-center justify-center bg-[#FDA855]'
+            style={{
+                position: "fixed",
+                width: "100%",
+                height: "180px",
+                zIndex: "9999",
+            }}
+        >
+            <div className='md:flex md:flex-row md:justify-between flex flex-col items-center md:xl:gap-72 md:gap-20 md:items-center'>
+                <div>
+                    <Link href='/' className='flex items-center'>
+                        <img
+                            src='/logo/Logo.png'
+                            className='md:h-20 md:mr-3 h-10'
+                            alt='Logo'
+                        />
+                    </Link>
+                </div>
+                <div className='flex flex-col items-center md:flex md:flex-col md:items-center md:gap-5'>
+                    <ul
+                        style={{
+                            color: "black",
+                            fontSize: "20px",
+                            fontFamily: "Rubik",
+                            fontWeight: "700",
+                            lineHeight: "30px",
+                            letterSpacing: "0.10px",
+                            wordWrap: "break-word",
+                        }}
+                        className='flex gap-[30px] '
+                    >
+                        <li>
+                            <Link href='/events'>Events</Link>
+                        </li>
+                        <li>
+                            <Link href='/about'>About</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>How it works</Link>
+                        </li>
+                    </ul>
+                    <div>
+                        <p
+                            style={{
+                                color: "#878787",
+                                fontSize: "17px",
+                                fontFamily: "Rubik",
+                                fontWeight: "300",
+                                lineHeight: "30px",
+                                letterSpacing: "0.10px",
+                                wordWrap: "break-word",
+                            }}
+                        >
+                            © Pebble Work, 2023
+                        </p>
+                    </div>
+                </div>
+                <div className='md:flex '>
+                    <ul className='flex gap-20 md:gap-8'>
+                        <li>
+                            <Link href='#'>
+                                <img src='/icons/Facebook.png' alt='Facebook' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <img src='/icons/Google.png' alt='Gmail' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <img src='/icons/Twitter.png' alt='Twitter' />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,185 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Navbar = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [menuDropdownOpen, setMenuDropdownOpen] = useState(false);
+
+    const menuDropdown = () => {
+        setMenuDropdownOpen(!menuDropdownOpen);
+    };
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
+        <nav className='border border-gray-200 dark:bg-gray-900 xl:flex xl:flex-col xl:items-center'>
+            <div
+                style={{
+                    filter: "drop-shadow(5px 25px 60px #000000)",
+                    position: "fixed",
+                    width: "100%",
+                    zIndex: "9999",
+                }}
+                className='max-w-screen-xl rounded-b-[45px] bg-opacity-30 bg-white flex flex-wrap items-center justify-between p-4 xl:gap-40'
+            >
+                <Link href='./' className='flex items-center'>
+                    <img
+                        src='/logo/Logo.png'
+                        className='h-8 mr-3'
+                        alt='Pebble Logo'
+                    />
+                </Link>
+                <div className='flex items-center md:order-2'>
+                    <div className='flex gap-4'>
+                        <button className='inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md'>
+                            Sign in
+                        </button>
+                        <button className='inline-flex items-center px-4 py-2 bg-[#2F7DA9] hover:bg-[#62b1de] text-white text-sm font-medium rounded-md'>
+                            Sign up
+                        </button>
+                    </div>
+                    <button
+                        type='button'
+                        onClick={toggleDropdown}
+                        className='inline-flex items-center font-medium justify-center px-4 text-sm text-gray-900  rounded-lg cursor-pointer'
+                    >
+                        <div className='flex items-center'>
+                            <img src='/icons/Web.png' alt='Pebble Logo' />
+                            <p className='text-black'>(EN)</p>
+                        </div>
+                    </button>
+                    <div
+                        className={`z-50 ${
+                            isDropdownOpen ? "block" : "hidden"
+                        } my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700`}
+                        id='language-dropdown-menu'
+                    >
+                        <ul className='py-10 font-medium flex absolute right-[3px]'>
+                            <li>
+                                <Link
+                                    href='#'
+                                    className='block px-4 py-2 text-sm text-gray-700 hover:border hover:rounded-full dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white'
+                                >
+                                    <div className='inline-flex items-center'>
+                                        <svg
+                                            aria-hidden='true'
+                                            className='h-3.5 w-3.5 rounded-full mr-2'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            id='flag-icon-css-us'
+                                            viewBox='0 0 512 512'
+                                        >
+                                            <path
+                                                fill='#3c3b6e'
+                                                d='M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0z'
+                                                transform='scale(3.9385)'
+                                            />
+                                            <path
+                                                fill='#192f5d'
+                                                d='M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0z'
+                                                transform='scale(3.9385)'
+                                            />
+                                            <path
+                                                fill='#fff'
+                                                d='M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0z'
+                                                transform='scale(3.9385)'
+                                            />
+                                        </svg>
+                                        English (EN)
+                                    </div>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href='#'
+                                    className='block px-4 py-2 text-sm text-gray-700 hover:border hover:rounded-full dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white'
+                                >
+                                    <div className='inline-flex items-center'>
+                                        <img
+                                            className='mr-1'
+                                            src='/Palestine.png'
+                                            width='20px'
+                                            height='20px'
+                                        />
+                                        Arabic (AR)
+                                    </div>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <button
+                        data-collapse-toggle='navbar-language'
+                        type='button'
+                        onClick={menuDropdown}
+                        className='inline-flex z-50 items-center p-2 ml-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                        aria-controls='navbar-language'
+                        aria-expanded='false'
+                    >
+                        <svg viewBox='0 0 100 80' width='40' height='40'>
+                            <rect
+                                fill='grey'
+                                width='100'
+                                height='15'
+                                rx='10'
+                            ></rect>
+                            <rect
+                                fill='grey'
+                                y='25'
+                                width='100'
+                                height='15'
+                                rx='10'
+                            ></rect>
+                            <rect
+                                fill='grey'
+                                y='50'
+                                width='100'
+                                height='15'
+                                rx='10'
+                            ></rect>
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    className={`${
+                        menuDropdownOpen ? "block" : "hidden"
+                    } md:block md:flex md:items-center w-full md:w-auto`}
+                    id='navbar-language'
+                >
+                    <ul
+                        style={{ fontFamily: "Rubik" }}
+                        className='md:static text-center md:bg-transparent bg-white gap-3 absolute w-full flex flex-col font-medium  md:p-0 border border-gray-300 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 dark:border-gray-700 left-[0px] top-0 z-10'
+                        role='menu'
+                    >
+                        <li>
+                            <Link
+                                href='#'
+                                className='block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700'
+                            >
+                                Events
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='/about'
+                                className='block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700'
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='#'
+                                className='block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700'
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
