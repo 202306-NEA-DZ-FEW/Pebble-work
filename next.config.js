c8cff37ee6e94525dffba292e17682288ebf3839
Polichinell <tsuqinoa@gmail.com>
2023-10-15 15:26:39 +0200

    feat(events): create a function to upload and render new events
    Using firestore and Cloud storage, event's data including images are stored in firebase and then
    rendered in their own page

--- next.config.js
+++ next.config.js
@@ -7,4 +7,7 @@ module.exports = {
         dirs: ["src"],
     },
     reactStrictMode: true,
+    images: {
+        domains: ['firebasestorage.googleapis.com'], // Add the Firebase Cloud Storage hostname
+    },
 };
