5612f79d9745993e87823421fca6711ce00dad82
takidilmi <angeldilmi@gmail.com>
2023-10-20 02:03:20 +0200

    style(homepage): add new animation to the MainCard from homepage

--- src/components/Homecards/MainCard.jsx
+++ src/components/Homecards/MainCard.jsx
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import styles from "@/styles/Homepage.module.css";
 
 const FeatureCard = ({ title, description, buttonText, imageUrl, link }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    // handle scroll event and update the visibility of the featuredcard
+    useEffect(() => {
+        const handleScroll = () => {
+            // Get the current scroll position
+            const scrollPosition = window.scrollY;
+            // Get the height of the window
+            const windowHeight = window.innerHeight;
+            // Get the featuredcard element by its ID
+            const element = document.getElementById("featuredCard");
+
+            // Check if the element exists
+            if (element) {
+                // Get the position of the element relative to the top of the document
+                const elementPosition = element.offsetTop;
+
+                // Check if the scroll position is greater than half of the window height from the top of the element
+                if (scrollPosition > elementPosition - windowHeight / 2) {
+                    // Set the visibility of the featured card to true
+                    setIsVisible(true);
+                }
+            }
+        };
+
+        // Add scroll event listener and call the handleScroll function
+        window.addEventListener("scroll", handleScroll);
+
+        // Clean up by removing the scroll event listener when the component is unmounted
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     const handleButtonClick = () => {
         window.location.href = link;
     };
 
     return (
-        <div className='w-1/3 sm:w-1/2 md:w-1/3 flex flex-col justify-evenly items-center'>
-            <div className='xl:w-[216px] xl:h-[245.75px] lg:w-[177px] lg:h-[200px] md:w-[131px] md:h-[145px] sm:w-[100px] sm:h-[110px] w-[80px] h-[80px] '>
+        <div
+            id='featuredCard'
+            className={`w-1/3 sm:w-1/2 md:w-1/3 flex flex-col justify-evenly items-center ${
+                styles.featuredCard
+            } ${isVisible ? styles.visible : styles.invisible}`}
+        >
+            <div
+                className={`xl:w-[216px] xl:h-[245.75px] lg:w-[177px] lg:h-[200px] md:w-[131px] md:h-[145px] sm:w-[100px] sm:h-[110px] w-[80px] h-[80px] ${styles.cardImage}`}
+            >
                 <img src={imageUrl} alt='Card Image' />
             </div>
             <div className='w-full text-center px-2'>
-                <div className="text-zinc-900 w- lg:text-[32px] md:text-[24px] sm:text-[20px] font-medium font-['Rubik'] sm:leading-[44px] tracking-[0.10px]">
+                <div
+                    className={`text-zinc-900 w- lg:text-[32px] md:text-[24px] sm:text-[20px] font-medium ... ... ... font-['Rubik'] sm:leading-[44px] tracking-[0.10px] ${styles.title}`}
+                >
                     {title}
                 </div>
-                <div className="text-zinc-500 lg:text-[18px] md:text-[15px] sm:text-[13px] text-[10px] font-normal font-['Rubik'] sm:leading-[30px] tracking-[0.10px]">
+                <div
+                    className={`text-zinc-500 lg:text-[18px] md:text-[15px] sm:text-[13px] ... ... ... text-[10px] font-normal font-['Rubik'] sm:leading-[30px] tracking-[0.10px] ${styles.description}`}
+                >
                     {description}
                 </div>
             </div>
             <Link
                 href=''
-                className="xl:w-[163px] xl:h-[52px] justify-start items-center gap-2.5 inline-flex text-zinc-900 lg:text-[22px] sm:text-[16px] font-medium font-['Rubik'] leading-[30px] tracking-tight"
+                className={`xl:w-[163px] ... ... ... xl:h-[52px] justify-start items-center gap-2.5 inline-flex text-zinc-900 lg:text-[22px] sm:text-[16px] font-medium font-['Rubik'] leading-[30px] tracking-tight ${styles.button}`}
                 onClick={handleButtonClick}
             >
                 {buttonText}
-                {/* <div className='w-6 h-6 relative' /> */}
             </Link>
         </div>
     );
@@ -35,14 +80,14 @@ const MainCard = () => (
     <div className='w-full bg-white flex flex-wrap justify-start items-start'>
         <FeatureCard
             title='Join the community!'
-            description='Body placeholder for text paragraph, a paragraph is a self-contained unit of text description.'
+            description='Body placeholder for text paragraph, a ... paragraph ... is ... a self-contained unit of text description.'
             buttonText='Sign up'
             imageUrl='/Homepage/ManTop.png'
             link='/signup'
         />
         <FeatureCard
             title='Attend events'
-            description='Body placeholder for text paragraph, a paragraph is a self-contained unit of text description.'
+            description='Body placeholder for text paragraph, a paragraph is a self-contained unit of text ... description.'
             buttonText='View events'
             imageUrl='/Homepage/attend.png'
             link='/events'
