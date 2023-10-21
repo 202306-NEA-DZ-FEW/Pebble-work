da4863d11d1daaee132a3ae16ff0516a4fb870e8
takidilmi <angeldilmi@gmail.com>
2023-10-21 19:16:09 +0200

    fix(github prettier check): fix github prettier check not passing

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ const path = require("path");
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
-  i18n: {
-    ...i18n,
-    // Add the additional properties here
-    localePath: path.resolve("./public/locales"),
-    defaultNS: "common",
-    serializeConfig: false,
-  },
-  eslint: {
-    dirs: ["src"],
-  },
-  reactStrictMode: true,
-  images: {
-    domains: ["firebasestorage.googleapis.com"],
-  },
+    i18n: {
+        ...i18n,
+        // Add the additional properties here
+        localePath: path.resolve("./public/locales"),
+        defaultNS: "common",
+        serializeConfig: false,
+    },
+    eslint: {
+        dirs: ["src"],
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ["firebasestorage.googleapis.com"],
+    },
 };
