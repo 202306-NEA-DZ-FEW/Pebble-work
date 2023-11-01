c2560d77ea5093dc3109617bb6f53bca99449af8
halla <aniabisso.16@gmail.com>
2023-11-01 11:28:11 +0100

    feat(home page): add translation to home page

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useTranslation } from "next-i18next";
 import GalleryCard from "@/components/Homecards/GalleryCard";
 import HeaderCard from "@/components/Homecards/HeaderCard";
 import HeroCard from "@/components/Homecards/HeroCard";
@@ -9,27 +9,46 @@ import SocialCard from "@/components/Homecards/SocialCard";
 
 const CardContainer = ({ children }) => <div className=''>{children}</div>;
 
-export const Heading = ({ text }) => (
-    <h2 className='text-center lg:text-[36px] md:text-[28px] sm:text-[22px] text-[18px] mt-4 sm:mt-8 md:mt-12 lg:mt-16 text-black font-[500]'>
-        {text}
-    </h2>
-);
+export const Heading = ({ text }) => {
+    const { t } = useTranslation();
+
+    return (
+        <h2 className='text-center lg:text-[36px] md:text-[28px] sm:text-[22px] text-[18px] mt-4 sm:mt-8 md:mt-12 lg:mt-16 text-black font-[500]'>
+            {t(text)}
+        </h2>
+    );
+};
 
 const HomeCards = () => {
+    const { t } = useTranslation();
+
     return (
         <div className='container overflow-hidden flex flex-col items-center justify-center mx-auto'>
             <HeaderCard />
 
-            <Heading text='How It Works' />
+            <Heading />
+            <p
+                className='mt-4 sm:mt-8 md:mt-12 lg:mt-16'
+                style={{ fontSize: "42px", color: "black" }}
+            >
+                {t("common:homeCards:howItWorks")}
+            </p>
             <MainCard />
 
-            <Heading text='Sustainable Development Goals' />
+            <Heading />
+            <p
+                className='mt-4 sm:mt-8 md:mt-12 lg:mt-16'
+                style={{ fontSize: "42px", color: "black" }}
+            >
+                {t("common:homeCards:sustainableDevelopmentGoals")}
+            </p>
+            <p className='mt-4 sm:mt-8 md:mt-12 lg:mt-16'>
+                {t("common:homeCards:introductorySentence")}
+            </p>
             <GalleryCard />
 
-            <Heading text='Hear It from Volunteers' />
-            <p className='mt-4 sm:mt-8 md:mt-12 lg:mt-16'>sentences here...</p>
-
-            <div className='xl:h-[420px] lg:h-[420px] sm:mb-20 '>
+            <p className='mt-4 sm:mt-8 md:mt-12 lg:mt-16'></p>
+            <div className='xl:h-[420px] lg:h-[420px] sm:mb-20'>
                 <SocialCard />
             </div>
 
