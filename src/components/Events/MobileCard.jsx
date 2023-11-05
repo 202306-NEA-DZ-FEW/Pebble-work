0d897500bedd5a1343829f7ca2bec536fd0637b9
takidilmi <angeldilmi@gmail.com>
2023-11-05 17:46:39 +0100

    style(mobilecard): add some styling to the mobilecard

--- src/components/Events/MobileCard.jsx
+++ src/components/Events/MobileCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-const MobileCard = (
+const Test = ({
     eventId,
     title,
     type,
@@ -9,23 +9,30 @@ const MobileCard = (
     location,
     date,
     time,
-    description
-) => {
+    description,
+}) => {
     const router = useRouter();
     const handleReviewClick = () => {
         router.push(`/events/${eventId}`);
     };
     return (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center text-black items-center'>
+            <h1>{title}</h1>
             <div
                 className='w-[250px] h-[250px]'
                 style={{
                     backgroundImage: `url(${image})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
                 }}
             >
                 <p className='text-end'>avatars</p>
+                <h3>{location}</h3>
+                <h3>{date}</h3>
+                <h3>{time}</h3>
             </div>
-            <h1 className='w-[250px] h-[30px] text-center'>title</h1>
+            <h2 className='w-[250px] h-[30px] text-center'>{type}</h2>
             <button
                 className={`w-[250px] h-[30px] bg-blue-400 hover:bg-blue-500 text-white text-[14px] rounded-[4px]`}
                 onClick={handleReviewClick}
@@ -36,4 +43,4 @@ const MobileCard = (
     );
 };
 
-export default MobileCard;
+export default Test;
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,14 +1,12 @@
 import React from "react";
+import SmallMobileEvents from "@/components/Events/SmallMobileEvents";
 
 import MobileCard from "@/components/Events/MobileCard";
 
 const Test = () => {
     return (
         <>
-            <div className='flex flex-row flex-wrap gap-2 justify-center'>
-                <MobileCard />
-                <MobileCard />
-            </div>
+            <SmallMobileEvents />
         </>
     );
 };
