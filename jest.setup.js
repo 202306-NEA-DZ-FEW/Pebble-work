3142457d1fac962e0d6a30a411924f958c32a40b
takidilmi <angeldilmi@gmail.com>
2023-11-07 02:48:52 +0100

    test(jest): add some settings to the jest setup, most used ones and needed to be mocked

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
 import "@testing-library/jest-dom/extend-expect";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
-
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-fs-backend";
 jest.mock("next/router", () => ({
     useRouter: jest.fn(),
 }));
@@ -11,3 +13,16 @@ jest.mock("next-i18next", () => ({
         t: (key) => key,
     }),
 }));
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+    supportedLngs: ["en", "tr", "de", "zh"],
+    nonExplicitSupportedLngs: true,
+    backend: {
+      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+    },
+  });
