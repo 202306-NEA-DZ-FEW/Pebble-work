54e122ee17cfb0f8b5d415d8bc357ef7608cabb1
takidilmi <angeldilmi@gmail.com>
2023-11-12 02:53:29 +0100

    style(techstack): change the animation for the techstack in about page

--- src/components/Cards/Aboutus/TechStack.jsx
+++ src/components/Cards/Aboutus/TechStack.jsx
@@ -12,15 +12,13 @@ const TechStack = () => {
 
     const timer = useRef(); // Move timer inside the component
 
-    // Wrap the handlers with useCallback to avoid creating new functions on each render
     const handleMouseOver = useCallback(() => {
         timer.current = setTimeout(() => {
+            // Pause the animation of the container
             if (containerRef.current) {
                 containerRef.current.style.animationPlayState = "paused";
             }
-            if (pngRef.current) {
-                pngRef.current.style.animationPlayState = "paused";
-            }
+            // Pause the animation of each SVG
             svgRefs.current.forEach((ref) => {
                 if (ref.current) {
                     ref.current.style.animationPlayState = "paused";
@@ -33,9 +31,6 @@ const TechStack = () => {
         clearTimeout(timer.current);
         if (containerRef.current) {
             containerRef.current.style.animationPlayState = "running";
-        }
-        if (pngRef.current) {
-            pngRef.current.style.animationPlayState = "running";
         }
         svgRefs.current.forEach((ref) => {
             if (ref.current) {
@@ -51,170 +46,182 @@ const TechStack = () => {
         };
     }, []);
     return (
-        <div className={`z-10 ${styles.container}`} ref={containerRef}>
-            <div
-                ref={svgRefs.current[0]}
-                className={`${styles.tooltip} ${styles.svg} `}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-            >
-                <img src='/Tech/icons8-css.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>CSS3</h3>
-                        <ul>
-                            <li>Style sheet language for web</li>
-                            <li>Beautiful, responsive, interactive</li>
-                            <li>Enhanced design possibilities</li>
-                        </ul>
+        <>
+            <div className={`${styles.container}`}>
+                <div className={styles.svgContainer} ref={containerRef}>
+                    <div
+                        ref={svgRefs.current[0]}
+                        className={`${styles.tooltip} ${styles.svg} `}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                    >
+                        <img src='/Tech/icons8-css.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>CSS3</h3>
+                                <ul>
+                                    <li>Style sheet language for web</li>
+                                    <li>Beautiful, responsive, interactive</li>
+                                    <li>Enhanced design possibilities</li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[1]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/icons8-firebase.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>firebase</h3>
-                        <ul>
-                            <li>Backend platform for apps</li>
-                            <li>Secure, scalable, reliable</li>
-                            <li>Seamless integration with web</li>
-                        </ul>
+                    <div
+                        ref={svgRefs.current[1]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/icons8-firebase.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>firebase</h3>
+                                <ul>
+                                    <li>Backend platform for apps</li>
+                                    <li>Secure, scalable, reliable</li>
+                                    <li>Seamless integration with web</li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <img
-                className={styles.png}
-                src='/Tech/techstack.png'
-                ref={pngRef}
-            />
-            <div
-                className={`${styles.tooltip} ${styles.svg}`}
-                ref={svgRefs.current[2]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-            >
-                <img src='/Tech/icons8-nextjs.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>Next.js</h3>
-                        <ul>
-                            <li>React framework for web</li>
-                            <li>Fast, easy, versatile</li>
-                            <li>Enjoyable development experience</li>
-                        </ul>
+
+                    <div
+                        className={`${styles.tooltip} ${styles.svg}`}
+                        ref={svgRefs.current[2]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                    >
+                        <img src='/Tech/icons8-nextjs.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>Next.js</h3>
+                                <ul>
+                                    <li>React framework for web</li>
+                                    <li>Fast, easy, versatile</li>
+                                    <li>Enjoyable development experience</li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[3]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/icons8-tailwind-css.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>TailwindCSS</h3>
-                        <ul>
-                            <li>Utility-first CSS framework</li>
-                            <li>Customizable, modular, efficient</li>
-                            <li>Rapid prototyping and development</li>
-                        </ul>
+                    <div
+                        ref={svgRefs.current[3]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/icons8-tailwind-css.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>TailwindCSS</h3>
+                                <ul>
+                                    <li>Utility-first CSS framework</li>
+                                    <li>Customizable, modular, efficient</li>
+                                    <li>Rapid prototyping and development</li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[4]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/jest-svgrepo.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>Jest</h3>
-                        <ul>
-                            <li>JavaScript testing framework</li>
-                            <li>Simple, fast, powerful</li>
-                            <li>Confident code quality assurance</li>
-                        </ul>
+                    <div
+                        ref={svgRefs.current[4]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/jest-svgrepo.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>Jest</h3>
+                                <ul>
+                                    <li>JavaScript testing framework</li>
+                                    <li>Simple, fast, powerful</li>
+                                    <li>Confident code quality assurance</li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[5]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/i18next.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>I18next</h3>
-                        <ul>
-                            <li>
-                                Internationalization framework for JavaScript
-                            </li>
-                            <li>Learn once, translate everywhere</li>
-                            <li>
-                                Complete translation solution for web, mobile
-                                and desktop
-                            </li>
-                        </ul>
+                    <div
+                        ref={svgRefs.current[5]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/i18next.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>I18next</h3>
+                                <ul>
+                                    <li>
+                                        Internationalization framework for
+                                        JavaScript
+                                    </li>
+                                    <li>Learn once, translate everywhere</li>
+                                    <li>
+                                        Complete translation solution for web,
+                                        mobile and desktop
+                                    </li>
+                                </ul>
+                            </div>
+                            <i></i>
+                        </span>
                     </div>
-                    <i></i>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[6]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/material-ui.svg' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>Material UI</h3>
-                        <ul>
-                            <li>React components based on Material Design</li>
-                            <li>Beautiful, responsive, customizable</li>
-                            <li>Comprehensive and production-ready library</li>
-                        </ul>
-                        <i></i>
+                    <div
+                        ref={svgRefs.current[6]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/material-ui.svg' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>Material UI</h3>
+                                <ul>
+                                    <li>
+                                        React components based on Material
+                                        Design
+                                    </li>
+                                    <li>Beautiful, responsive, customizable</li>
+                                    <li>
+                                        Comprehensive and production-ready
+                                        library
+                                    </li>
+                                </ul>
+                                <i></i>
+                            </div>
+                        </span>
                     </div>
-                </span>
-            </div>
-            <div
-                ref={svgRefs.current[7]}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                className={`${styles.tooltip} ${styles.svg}`}
-            >
-                <img src='/Tech/DaisyUi.webp' />
-                <span className={styles.right}>
-                    <div className={styles.textContent}>
-                        <h3>daisyUI</h3>
-                        <ul>
-                            <li>Tailwind CSS component library</li>
-                            <li>No JavaScript, pure CSS</li>
-                            <li>Cute and functional design</li>
-                        </ul>
-                        <i></i>
+                    <div
+                        ref={svgRefs.current[7]}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className={`${styles.tooltip} ${styles.svg}`}
+                    >
+                        <img src='/Tech/DaisyUi.webp' />
+                        <span className={styles.right}>
+                            <div className={styles.textContent}>
+                                <h3>daisyUI</h3>
+                                <ul>
+                                    <li>Tailwind CSS component library</li>
+                                    <li>No JavaScript, pure CSS</li>
+                                    <li>Cute and functional design</li>
+                                </ul>
+                                <i></i>
+                            </div>
+                        </span>
                     </div>
-                </span>
+                </div>
+                <img
+                    className={styles.png}
+                    src='/Tech/techstack.png'
+                    ref={pngRef}
+                />
             </div>
-        </div>
+        </>
     );
 };
 
