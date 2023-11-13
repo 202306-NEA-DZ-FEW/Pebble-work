0ccc8a77fd41845df2996dd2017c3921a2c7fd69
takidilmi <angeldilmi@gmail.com>
2023-11-14 00:11:52 +0100

    fix(translation): attempt "1" to fix the issue of error 500 on vercel cause of i18next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require("path");
 module.exports = {
     i18n: {
         defaultLocale: "en",
         locales: ["en", "tr", "de", "zh"],
+        localePath: path.resolve("./public/locales"),
+        defaultNS: "common",
+        serializeConfig: false,
     },
 };
