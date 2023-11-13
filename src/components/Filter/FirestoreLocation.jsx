9e3f4a81d6784dcaf7a7aa9c26c10e598d872cdb
hasna <manelhasnahaddoud@gmail.com>
2023-11-13 23:04:30 +0100

    fix(firestorelocation): fix the bug of fetching data in the modal
    fix the bug of fetching data in the modal

--- src/components/Filter/FirestoreLocation.jsx
+++ src/components/Filter/FirestoreLocation.jsx
@@ -1,20 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { db } from "@/util/firebase";
 import { doc, getDoc, collection } from "firebase/firestore";
-import Modal from "react-awesome-modal";
 function FirestoreLocation({ onInputChange, inputValue1, setInputValue1 }) {
-    // Open state
-    const [open, setOpen] = useState(false);
-
-    // Modal handler
-    const openModal = () => {
-        setOpen(true);
-    };
-
-    const closeModal = () => {
-        setOpen(false);
-    };
-
     const [filteredLocations, setFilteredLocations] = useState([]);
     // const [isFormVisible, setFormVisible] = useState(false);
     // const buttonRef = useRef(null);
@@ -58,8 +45,10 @@ function FirestoreLocation({ onInputChange, inputValue1, setInputValue1 }) {
             const locationsDoc = await getDoc(locationsDocRef);
             if (locationsDoc.exists()) {
                 const locationData = locationsDoc.data();
-                setFilteredLocations(locationData);
-                console.log(filteredLocations);
+                const locationsArray = [].concat(
+                    ...Object.values(locationData)
+                );
+                setFilteredLocations(locationsArray);
             }
         } catch (error) {
             console.error("Error fetching locations:", error);
@@ -72,76 +61,46 @@ function FirestoreLocation({ onInputChange, inputValue1, setInputValue1 }) {
 
     return (
         <>
-            {/* <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Location</button>
-<dialog id="my_modal_1" className="modal">
-
-  <div className="modal-box">
-
-    <h3 className="font-bold text-lg">Modal Title</h3>
-
-    <div className="py-4">
-
-      <ul className='list-none'>
-
-        {Object.entries(filteredLocations).map(( [key, value], index) => (
-
-          <li key={index} className='my-2 block'>
-
-            {value}
-
-          </li>
-
-        ))}
-
-      </ul>
-
-    </div>
-
-    <div className="modal-action">
-
-      <form method="dialog">
-
-        <button className="btn" onClick={() => document.getElementById('my_modal_1').close()}>Close</button>
-
-      </form>
-
-    </div>
-
-  </div>
-
-</dialog> */}
-
-            <button onClick={openModal}>Open Modal</button>
-
-            <Modal
-                visible={open}
-                width='400'
-                effect='fadeInUp'
-                onClickAway={closeModal}
-                styles={{
-                    width: "600px",
-                    height: "400px",
-                    border: "1px solid #ccc",
-                    borderRadius: "10px",
-                }}
-                className=''
+            <button
+                className='btn'
+                onClick={() =>
+                    document.getElementById("my_modal_1").showModal()
+                }
             >
-                <h3>Modal Title</h3>
+                Location
+            </button>
+            <dialog id='my_modal_1' className='modal rounded-tr rounded-br'>
+                <div className='modal-box'>
+                    <h3 className='font-bold text-lg mb-0'>Citys </h3>
 
-                <ul className=' flex flex-col my-2 mr-4 ml-4'>
-                    {/* map through data */}
+                    <div className=' mt-2  '>
+                        <ul className='list-none '>
+                            {Object.entries(filteredLocations).map(
+                                ([key, value], index) => (
+                                    <li key={index} className='my-2 block  '>
+                                        {value}
+                                    </li>
+                                )
+                            )}
+                        </ul>
+                    </div>
 
-                    {Object.entries(filteredLocations).map(
-                        ([key, value], index) => (
-                            <li key={index} className='my-2 '>
-                                {value}
-                            </li>
-                        )
-                    )}
-                </ul>
-
-                <button onClick={closeModal}>Close</button>
-            </Modal>
+                    <div className='modal-action'>
+                        <form method='dialog'>
+                            <button
+                                className='btn'
+                                onClick={() =>
+                                    document
+                                        .getElementById("my_modal_1")
+                                        .close()
+                                }
+                            >
+                                Close
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </>
     );
 }
