e388d3942349b3799968a695181d99e453325d54
takidilmi <angeldilmi@gmail.com>
2023-11-18 01:46:40 +0100

    style(homepage): add button transition animation on hover

--- src/components/Homecards/HeroCard.jsx
+++ src/components/Homecards/HeroCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
+import styles from "@/styles/Homepage.module.css";
 
 function HeroCard() {
     const { t } = useTranslation();
@@ -20,14 +21,14 @@ function HeroCard() {
                     </div>
                     <div className=' flex flex-col sm:flex-row justify-center items-center gap-6'>
                         <div className='cursor-pointer'>
-                            <div
-                                className='text-2xl font-medium leading-10 tracking-tight bg-cyan-700 text-white px-4 py-2 rounded-lg'
+                            <button
+                                className={`buttonTransition text-2xl font-medium leading-10 tracking-tight text-white px-4 py-2 rounded-lg`}
                                 onClick={() =>
                                     handleButtonClick("/target-page-1")
                                 }
                             >
                                 {t("common:heroCard:getStarted")}
-                            </div>
+                            </button>
                         </div>
                         <div className='cursor-pointer'>
                             <div
