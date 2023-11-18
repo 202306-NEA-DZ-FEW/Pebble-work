2cb029f9544d23454c9b06626751d1a77108a8fe
Polichinell <tsuqinoa@gmail.com>
2023-11-18 16:57:29 +0100

    style(profile): galaxy Fold responsiveness
    fix #179

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -69,15 +69,15 @@ const ProfilePage = () => {
         return <p>Loading</p>;
     } else {
         return (
-            <div className='pt-5 flex flex-col items-center justify-center'>
-                <h1 className='font-semibold text-lg md:text-4xl md:mt-5'>
+            <div className='pt-5 flex flex-col items-center justify-center sm-types'>
+                <h1 className='sm-text font-semibold text-lg md:text-4xl md:mt-5'>
                     {t("profile:yourProfile")}
                 </h1>
 
-                <div className='flex flex-row mt-4'>
+                <div className='sm-col flex flex-row mt-4'>
                     <div className='flex flex-col ml-4 mt-4 md:w-full md:gap-10'>
                         {/* Row to be changed Start */}
-                        <div className='flex flex-col md:flex-row items-center justify-around'>
+                        <div className='sm-col flex flex-col md:flex-row items-center justify-around'>
                             {" "}
                             {/* Updated */}
                             <div className='flex justify-center items-center h-2/12 w-4/12 rounded-full outline outline-2 overflow-hidden md:w-4/12 h-5/12 md:mt-8 mb-[3rem]'>
@@ -93,9 +93,9 @@ const ProfilePage = () => {
                                     className='h-full w-full rounded-full'
                                 />
                             </div>
-                            <div className='flex flex-row items-center justify-around gap-x-[5rem] pr-7'>
+                            <div className='sm-col flex flex-row items-center justify-around gap-x-[5rem] pr-7'>
                                 {/* Name and Surname */}
-                                <div className='md:w-3/12 md:mt-10'>
+                                <div className='sm-col-2 md:w-3/12 md:mt-10'>
                                     <h3 className='font-semibold text-lg text-gray-600'>
                                         {t("profile:Name")}:
                                     </h3>
@@ -111,7 +111,7 @@ const ProfilePage = () => {
                                 </div>
 
                                 {/* Email and Location */}
-                                <div className='md:w-3/12 md:mt-10'>
+                                <div className='sm-col-3 md:w-3/12 md:mt-10 text-left'>
                                     <h3 className='font-semibold text-lg text-gray-600'>
                                         {t("profile:Email")}:
                                     </h3>
@@ -130,12 +130,12 @@ const ProfilePage = () => {
                         {/* Row to be changed END */}
 
                         {/* Your Interests */}
-                        <h3 className='mt-8 font-semibold text-2xl text-gray-600 mb-2'>
+                        <h3 className='sm-button mt-8 font-semibold text-2xl text-gray-600 mb-2'>
                             {t("profile:interests")}:
                         </h3>
                         <div
                             className='grid grid-container grid-cols-2 gap-8 md:grid-cols-3 
-                        text-center justify-evenly md:w-full md:h-auto md:mt-1 md:text-3xl px-3'
+                        text-center justify-evenly md:w-full md:h-auto md:mt-1 md:text-3xl px-3 sm-types'
                         >
                             {EventTypes.map((type, index) => (
                                 <button
@@ -156,7 +156,7 @@ const ProfilePage = () => {
                 {/* Save Interests Button */}
                 <div className='mt-10'>
                     <button
-                        className='bg-orange-400 text-center h-10 w-30 px-4 py-2 text-xs text-white font-bold shadow-md md:h-14 mb-12 md:w-40 md:text-xl'
+                        className='sm-button bg-orange-400 text-center h-10 w-30 px-4 py-2 text-xs text-white font-bold shadow-md md:h-14 mb-12 md:w-40 md:text-xl'
                         onClick={handleEditProfile}
                     >
                         {t("profile:editProfile")}
@@ -178,7 +178,6 @@ export async function getStaticProps({ locale }) {
                 "edit",
                 "profile",
             ])),
-            // Will be passed to the page component as props
         },
     };
 }
