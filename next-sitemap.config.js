7e5423b72abedfd1b107a794cc7ab1b01806bf05
takidilmi <angeldilmi@gmail.com>
2023-11-23 15:27:31 +0100

    fix(prettier): fix prettier check on github

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    siteUrl: 'https://pebble-work.vercel.app',
+    siteUrl: "https://pebble-work.vercel.app",
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'], 
+    exclude: ["/server-sitemap.xml"],
     robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://pebble-work.vercel.app/server-sitemap.xml',
-      ],
+        additionalSitemaps: [
+            "https://pebble-work.vercel.app/server-sitemap.xml",
+        ],
     },
-  };
-  
+};
