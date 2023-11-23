4a15994a4753ee5437f116a7023f7d7871db8214
takidilmi <angeldilmi@gmail.com>
2023-11-23 15:27:03 +0100

    fix(sitemap): fix the issue of sitemap on vercel deployment

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,25 +1,11 @@
 module.exports = {
-    siteUrl: "https://pebble-work.vercel.app",
-    outDir: "./public", // specify the output directory here
+    siteUrl: 'https://pebble-work.vercel.app',
     generateRobotsTxt: true,
-    urls: [
-        {
-            url: "/",
-            lastModified: new Date(),
-            changeFrequency: "yearly",
-            priority: 1,
-        },
-        {
-            url: "/events",
-            lastModified: new Date(),
-            changeFrequency: "daily",
-            priority: 0.8,
-        },
-        {
-            url: "/about",
-            lastModified: new Date(),
-            changeFrequency: "weekly",
-            priority: 0.5,
-        },
-    ],
-};
+    exclude: ['/server-sitemap.xml'], 
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://pebble-work.vercel.app/server-sitemap.xml',
+      ],
+    },
+  };
+  
