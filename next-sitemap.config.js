fa7cfb799b7f19fc26cc141cde87428a03b29492
takidilmi <angeldilmi@gmail.com>
2023-11-25 17:27:11 +0100

    fix(siteampa): fix the problem with server-sitemap.xml error on google search

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: "https://pebble-work.vercel.app",
     generateRobotsTxt: true,
-    exclude: ["/server-sitemap.xml"],
+    // exclude: ["/server-sitemap.xml"],
     robotsTxtOptions: {
         additionalSitemaps: [
             "https://pebble-work.vercel.app/server-sitemap.xml",
